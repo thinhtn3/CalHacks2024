ed873b46176b93cb385d3d86b4abfea46b506ac5
thinhtn3 <thinhttnguyen@gmail.com>
2024-10-19 01:53:05 -0700

    Merging

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './LandingPage.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import LandingPage from './LandingPage.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LandingPage />
   </StrictMode>,
 )
