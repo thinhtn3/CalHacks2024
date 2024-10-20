248c93cd0a32aac418a2a072f92b63af6bf2c79b
王鑫浩 <wangxinhao@MacBook-Pro-2.local>
2024-10-19 17:28:08 -0700

    api ranking

--- backend/core/ranking.py
+++ backend/core/ranking.py
@@ -1,4 +1,38 @@
+import requests
+response = requests.get('https://edmtrain.com/api/events?client=40b7a9a1-1336-48bf-9055-de9d1abf9822')
 from geopy.distance import geodesic
+import random
+
+class Concert:
+    # location is tuple (latitude, longitude)
+    # artist is a set 
+    # prices is a number
+    # rating is from 1 to 5
+    def __init__(self, title, location, artist, imageURL):
+        self.title = title
+        self.rating = round(random.uniform(1, 5), 1)
+        self.location = location
+        self.artist = artist
+        self.prices = random.randint(100, 1000)
+        self.imageURL = imageURL
+    def __str__(self):
+        return "{0}".format(self.title)
+    def distance_difference(self, user):
+        return abs(geodesic(user.location, self.location).kilometers)
+    def artist_diffference(self, user):
+        total = len(self.artist)
+        matched = 0
+        for userLike in user.likedArtist:
+            for haveArtist in self.artist:
+                if userLike == haveArtist["name"]:
+                    match += 1
+        return total - matched
+    def prices_difference(self, user):
+        return abs(self.prices - user.budget)
+    def ranking_score(self, user, max_distance, max_prices):
+        return max(self.rating - (self.distance_difference(user) / max_distance) - \
+            (self.artist_diffference(user) / len(user.likedArtist)) - \
+            (self.prices_difference(user) / max_prices), 0)
 
 def max_const(arr_Concerts, user):
     max_prices = arr_Concerts[0]
@@ -10,37 +44,6 @@ def max_const(arr_Concerts, user):
             max_distance = Concert
     return (max_prices.prices, max_distance.distance_difference(user))
 
-
-class Concert:
-    # location is tuple (latitude, longitude)
-    # artist is a set 
-    # prices is a number
-    # rating is from 1 to 5
-    def __init__(self, title, rating, location, artist, prices):
-        self.title = title
-        self.rating = rating
-        self.location = location
-        self.artist = artist
-        self.prices = prices
-    def __str__(self):
-        return "{0}".format(self.title)
-    def distance_difference(self, user):
-        return abs(geodesic(user.location, self.location).kilometers)
-    def artist_diffference(self, user):
-        total = len(self.artist)
-        matched = 0
-        for art in user.likedArtist:
-            if art in self.artist:
-                matched += 1
-        return total - matched
-    def prices_difference(self, user):
-        return abs(self.prices - user.budget)
-    def ranking_score(self, user, max_distance, max_prices):
-        return self.rating - (self.distance_difference(user) / max_distance) - \
-            (self.artist_diffference(user) / len(user.likedArtist)) - \
-            (self.prices_difference(user) / max_prices)
-             
-
         
 
 class User:
@@ -49,49 +52,43 @@ class User:
         self.budget = budget
         self.likedArtist = likedArtist
 
-def quick_sort(arr, user, max_bpm, max_distance):
-    if len(arr) <= 0:
-        return arr
-    elif (arr) == 1:
-        return [(arr[0], arr[0].ranking_score(user, max_distance, max_prices))]
+def quick_sort(arr, user, max_prices, max_distance):
+    if len(arr) <= 1:
+        return [(x, x.ranking_score(user, max_distance, max_prices)) for x in arr]
     else:
-        # Select a pivot element (using the last element here for simplicity)
         pivot = arr[-1]
         left = []
         right = []
         pivot_score = pivot.ranking_score(user, max_distance, max_prices)
-        for Concert in arr[:-1]:
-            if Concert.ranking_score(user, max_distance, max_prices) > pivot_score:
-                left.append(Concert)
+        for concert in arr[:-1]:
+            if concert.ranking_score(user, max_distance, max_prices) > pivot_score:
+                left.append(concert)
             else:
-                right.append(Concert)
+                right.append(concert)
+        sorted_left = quick_sort(left, user, max_prices, max_distance)
+        sorted_right = quick_sort(right, user, max_prices, max_distance)
+        return sorted_left + [(pivot, pivot_score)] + sorted_right
             
-        
-        # Recursively apply quick_sort to 'left' and 'right', and combine with pivot
-        return quick_sort(left, user, max_bpm, max_distance) + [(pivot, pivot_score)] + quick_sort(right, user, max_bpm, max_distance)
-    
-concert1 = Concert("Rock in Rio", 5, (22.911014, -43.209373), {"Metallica", "Iron Maiden"}, 150)
-concert2 = Concert("Tomorrowland", 5, (51.091526, 4.385689), {"David Guetta", "Calvin Harris"}, 250)
-concert3 = Concert("Coachella", 4, (33.682222, -116.237222), {"Beyoncé", "The Weeknd"}, 399)
-concert4 = Concert("Lollapalooza", 4, (41.873076, -87.629167), {"Imagine Dragons", "Green Day"}, 130)
-concert5 = Concert("Glastonbury", 5, (51.158101, -2.583294), {"Coldplay", "Ed Sheeran"}, 275)
-concert6 = Concert("Woodstock", 4, (42.364538, -74.577132), {"Jimi Hendrix", "Santana"}, 80)
-concert7 = Concert("Sunburn Festival", 3, (15.299326, 74.123996), {"DJ Snake", "Afrojack"}, 120)
-concert8 = Concert("Electric Daisy Carnival", 5, (36.085600, -115.154083), {"Zedd", "Tiesto"}, 200)
-concert9 = Concert("Fuji Rock Festival", 5, (36.690147, 138.775326), {"Sia", "Halsey"}, 185)
-concert10 = Concert("Exit Festival", 4, (45.252869, 19.845175), {"Martin Garrix", "Steve Aoki"}, 90)
 
-arr = [concert1, concert2, concert3, concert4, concert5, concert6, concert7, concert8, concert9, concert10]
-user = User((37.7749, -122.4194), 100, {"Coldplay", "Ed Sheeran"})
-const_max = max_const(arr, user)
+
+concerts_json = response.json()["data"]
+all_concerts = []
+for concert in concerts_json:
+    if concert["name"] == None:
+        continue
+    venue = concert["venue"]
+    new_c = Concert(concert["name"], (venue["latitude"], venue["longitude"]), concert["artistList"], concert["link"])
+    all_concerts.append(new_c)
+
+user = User((37.7749, -122.4194), 300, {"Frank & Tony", "Adi", "Tal"})
+const_max = max_const(all_concerts, user)
 max_prices = const_max[0]
 max_distance = const_max[1]
-sorted_arr = quick_sort(arr, user, max_prices, max_distance)
-highest = sorted_arr[0][1]
+sorted_concerts = quick_sort(all_concerts, user, max_prices, max_distance)
 
+highest = sorted_concerts[0][1]
 final_result = []
-for concert in sorted_arr:
-
+for concert in sorted_concerts:
     percentage = concert[1] / highest * 100
     final_result.append((concert[0], percentage))
     print(final_result[-1][0])
